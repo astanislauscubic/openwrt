7e1e9593896a85721399f200366f5a9b3bd548b4
Byron Bradley <byronb@afterthoughtsoftware.com>
2015-06-17 22:07:14 +0100

    Add readme for 4.0.0-rc2 and release script

---
+++ wibe-release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+RELDIR=../releases/$1
+mkdir -p $RELDIR
+
+cp bin/ramips/openwrt-ramips-rt305x-wibe-4g-8M-squashfs-sysupgrade.bin $RELDIR/wibe-4g-8M-$1.bin
+cp wibe-readme $RELDIR/ReleaseNotes.txt
+cp build_dir/target*/net-snmp-*/mibs/WIBE-MIB.txt $RELDIR/WIBE-MIB.txt
+pushd $RELDIR
+zip ../wibe-$1.zip *
+popd
